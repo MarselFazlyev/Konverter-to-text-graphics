d31e0da2ff05ffc1847408572dd670649e2fd9ea
Fazlyev Marsel <fazlyev@mail.ru>
2022-07-24 18:36:21 +0300

    method convert in TextColorShemaImpl modified

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -4,24 +4,24 @@ import ru.netology.graphics.image.TextColorSchema;
 import ru.netology.graphics.image.TextColorShemaImpl;
 import ru.netology.graphics.image.TextGraphicsConverter;
 import ru.netology.graphics.image.TextGraphicsConverterImpl;
+import ru.netology.graphics.server.GServer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         TextGraphicsConverter converter = new TextGraphicsConverterImpl();
         TextColorSchema schema = new TextColorShemaImpl();
         converter.setTextColorSchema(schema);
         converter.setMaxRatio(2);
-        converter.setMaxHeight(255);
-        converter.setMaxWidth(150);
-        converter.convert("https://i.ibb.co/6DYM05G/edu0.jpg");
+        converter.setMaxHeight(150);
+        converter.setMaxWidth(255);
+//        converter.convert("https://i.ibb.co/6DYM05G/edu0.jpg");
 
-//        GServer server = new GServer(converter); // Создаём объект сервера
-//        server.start(); // Запускаем
+        GServer server = new GServer(converter); // Создаём объект сервера
+        server.start(); // Запускаем
 
 //         Или то же, но с выводом на экран:
-        String url = "https://i.ibb.co/6DYM05G/edu0.jpg";
-        String imgTxt = converter.convert(url);
-        System.out.println(imgTxt);
+//        String url = "https://i.ibb.co/6DYM05G/edu0.jpg";
+//        String imgTxt = converter.convert(url);
+//        System.out.println(imgTxt);
     }
 }
--- src/ru/netology/graphics/image/TextColorShemaImpl.java
+++ src/ru/netology/graphics/image/TextColorShemaImpl.java
@@ -1,9 +1,10 @@
 package ru.netology.graphics.image;
 
 public class TextColorShemaImpl implements TextColorSchema {
+    private final char[] symbols = {'#', '$', '@', '%', '*', '+', '-', '\''};
+
     @Override
     public char convert(int color) {
-        char[] symbols = {'#', '$', '@', '%', '*', '+', '-', '\''};
         return symbols[color / 32];
 
     }
